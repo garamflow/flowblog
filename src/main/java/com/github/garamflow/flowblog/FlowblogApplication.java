873968afc78ff86ce8e80ac0ac83d7364e056ec9
garamflow <garamflow@gmail.com>
2024-09-02 10:35:16 +0900

    chore: 프로젝트 생성 완료

---
+++ src/main/java/com/github/garamflow/flowblog/FlowblogApplication.java
@@ -0,0 +1,13 @@
+package com.github.garamflow.flowblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowblogApplication.class, args);
+    }
+
+}
