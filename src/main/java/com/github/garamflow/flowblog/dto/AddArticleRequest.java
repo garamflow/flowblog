9369fdd294c213c068eb7e9135b552a542be8796
garamflow <garamflow@gmail.com>
2024-09-17 00:18:25 +0900

    refactor: DTO를 record로 변경

--- src/main/java/com/github/garamflow/flowblog/dto/AddArticleRequest.java
+++ src/main/java/com/github/garamflow/flowblog/dto/AddArticleRequest.java
@@ -1,26 +1,18 @@
 package com.github.garamflow.flowblog.dto;
 
 import com.github.garamflow.flowblog.domain.Article;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-public class AddArticleRequest {
-
-    private String title;
-    private String content;
-    private String author;
-    private LocalDateTime createdAt;
-    private LocalDateTime updateAt;
-    private List<String> tags;
-    private String categoryName;
-
+public record AddArticleRequest(
+        String title,
+        String content,
+        String author,
+        LocalDateTime createdAt,
+        LocalDateTime updateAt,
+        List<String> tags,
+        String categoryName
+) {
     public Article toArticle() {
         return Article.builder()
                 .title(title)
--- src/main/java/com/github/garamflow/flowblog/dto/ArticleResponse.java
+++ src/main/java/com/github/garamflow/flowblog/dto/ArticleResponse.java
@@ -1,31 +1,29 @@
 package com.github.garamflow.flowblog.dto;
 
 import com.github.garamflow.flowblog.domain.Article;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Getter
-public class ArticleResponse {
-
-    private final Long id;
-    private final String title;
-    private final String author;
-    private final String content;
-    private final LocalDateTime createdAt;
-    private final LocalDateTime updatedAt;
-    private final List<String> tags;
-    private final String categoryName;
-
+public record ArticleResponse(
+        Long id,
+        String title,
+        String author,
+        String content,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt,
+        List<String> tags,
+        String categoryName
+) {
     public ArticleResponse(Article article) {
-        this.id = article.getId();
-        this.title = article.getTitle();
-        this.author = article.getAuthor();
-        this.content = article.getContent();
-        this.createdAt = article.getCreatedAt();
-        this.updatedAt = article.getUpdatedAt();
-        this.tags = article.getTags();
-        this.categoryName = article.getCategoryName();
+        this(
+                article.getId(),
+                article.getTitle(),
+                article.getAuthor(),
+                article.getContent(),
+                article.getCreatedAt(),
+                article.getUpdatedAt(),
+                article.getTags(),
+                article.getCategoryName()
+        );
     }
 }
