8a04519a12e37b5fd2c86d9f0fc65a66993e6498
garamflow <garamflow@gmail.com>
2024-09-23 15:02:10 +0900

    feat: 유저 로그인, 로그아웃 컨트롤러 구현

---
+++ src/main/java/com/github/garamflow/flowblog/controller/UserApiController.java
@@ -0,0 +1,33 @@
+package com.github.garamflow.flowblog.controller;
+
+import com.github.garamflow.flowblog.dto.AddUserRequest;
+import com.github.garamflow.flowblog.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public String signup(AddUserRequest request) {
+        userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
+
+        return "redirect:/login";
+    }
+}
